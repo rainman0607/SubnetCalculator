3d6448cb4985ace21c7aeb95322c66c609ab6312
rainman0607 <askemerci21@gmail.com>
2019-09-19 14:43:28 +0200

    created ip2bin and subnet validator

--- subnetcalc.py
+++ subnetcalc.py
@@ -1,7 +1,7 @@
 """
 TODO: Validation [X]
 TODO: Add argument parser []
-TODO: Mask loop []
+TODO: Mask loop [X]
 TODO: The calculation formula []
 TODO: Ranges []
 TODO: Wildcard mask []
@@ -11,10 +11,11 @@ TODO: Broadcast []
 
 class SubnetCalc:
 
-    def __init__(self, ip):
+    def __init__(self, ip, mask):
         self.ip = ip
+        self.mask = mask
 
-    def validate(self):
+    def ip_validate(self):
         # Split each octets into array
         octets = self.ip.split(".")
 
@@ -27,16 +28,42 @@ class SubnetCalc:
                 (0 <= int_octet_ip[1] <= 255) and \
                 (0 <= int_octet_ip[2] <= 255) and \
                 (0 <= int_octet_ip[3] <= 255):
-            return True
+            return int_octet_ip
         else:
             return False
 
+    def sub_validate(self):
+        oct_masks = [0, 128, 192, 224, 240, 248, 252, 254, 255]
 
-# False since it's a loopback
-x = SubnetCalc("127.0.0.1")
+        octet_subnet = [int(j) for j in self.mask.split(".")]
+        if (len(octet_subnet) == 4) and \
+                (octet_subnet[0] == 255) and \
+                (octet_subnet[1] in oct_masks) and \
+                (octet_subnet[2] in oct_masks) and \
+                (octet_subnet[3] in oct_masks) and \
+                (octet_subnet[0] >= octet_subnet[1] >= octet_subnet[2] >= octet_subnet[3]):
+            return octet_subnet
+        else:
+            return False
+
+    def ip2bin(self):
+        bin_arr = []
+        # Convert each IP Octet to Binary
+        oct2bin = [bin(i) for i in self.ip_validate()]
+        for i in range(0, len(oct2bin)):
+            # make each binary octet of 8 bit length by padding zeros
+            if len(oct2bin[i]) < 8:
+                pad = oct2bin[i].zfill(8)
+                bin_arr.append(pad)
+            else:
+                bin_arr.append(oct2bin[i])
+        return bin_arr
+
+
+# False since it's a loopback, and mask first octet is invalid
+x = SubnetCalc("127.0.0.1", "255.255.255.240")
 
 # True
-y = SubnetCalc("172.16.5.11")
+y = SubnetCalc("172.16.5.11", "255.255.255.240")
 
-print(x.validate())
-print(y.validate())
+print(y.ip2bin())
